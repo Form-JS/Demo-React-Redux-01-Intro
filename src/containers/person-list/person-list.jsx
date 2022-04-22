98c2f70554498a71c7b3ac8e5b214114dc00392a
Aurélien <aurelien.strimelle@bstorm.be>
2022-04-22 13:48:37 +0200

    Add feature remove person

--- src/containers/person-list/person-list.jsx
+++ src/containers/person-list/person-list.jsx
@@ -1,17 +1,27 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { AutoSizer, List } from 'react-virtualized';
+import { personRemove } from '../../store/actions/person-action';
+import style from './person-list.module.css';
 
 
 const PersonList = () => {
     const count = useSelector(state => state.person.count);
     const people = useSelector(state => state.person.list);
+    const dispatch = useDispatch();
 
     const personRow = ({ key, index }) => {
         const person = people[index];
 
+        const handleDelete = () => {
+            dispatch(personRemove(person.id));
+        };
+
         return (
-            <li key={key}>
-                {person.firstname} {person.lastname}
+            <li key={key} className={style.listItem}>
+                <p>
+                    {person.firstname} {person.lastname}
+                    <span onClick={handleDelete}>&#9587;</span>
+                </p>
             </li>
         );
     };
