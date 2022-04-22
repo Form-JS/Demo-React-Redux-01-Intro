de8e164aa221876641341b7daa5eeac159d70a02
Aurélien <aurelien.strimelle@bstorm.be>
2022-04-22 14:30:22 +0200

    Add DevTools Redux

--- src/store/store.js
+++ src/store/store.js
@@ -7,5 +7,7 @@ const reducer = combineReducers({
     person: personReducer
 });
 
-export const store = createStore(reducer);
+const devTools = (process.env.NODE_ENV === 'development') ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : undefined;
+export const store = createStore(reducer, devTools);
+
 export default store;
